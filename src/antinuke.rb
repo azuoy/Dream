b496d78e97558d33861941f074be1cd4589854fb
$o <100600051+azuoy@users.noreply.github.com>
2022-03-19 11:36:41 -0700

    Update antinuke.rb

--- src/antinuke.rb
+++ src/antinuke.rb
@@ -1,31 +1,4 @@
-#
-# MIT License
-#
-# Copyright (c) 2021 Carson Herrington
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-#====================================================================================================
-# KhajiitBot - NotArtyom - 2021
-# ----------------------------------------
-# Security and permissions functions
-#====================================================================================================
+.//made by $o
 
 def valid_json?(json)				# Detect if a file is valid JSON
 	buff = JSON.parse(json)
@@ -137,7 +110,7 @@ class Permit																		# Permit checking class
 	end
 end
 
-module Parser							# PARSE module for parsing user names and nicknames
+module Parser							# PARSE module for parsing user antinuke
 	module_function
 	def get_user(user, event=nil)
 		user = user.join(' ') if user.is_a?(Array)
@@ -305,4 +278,4 @@ def require_blacklist(event, post, blacklist)
 	end
 end
 
-#====================================================================================================
+	$o
