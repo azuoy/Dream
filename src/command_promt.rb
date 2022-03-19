1e9ea14fec64673d459692a5582d2da5c931471b
$o <100600051+azuoy@users.noreply.github.com>
2022-03-19 11:39:26 -0700

    Update command_promt.rb

--- src/command_promt.rb
+++ src/command_promt.rb
@@ -1,39 +1,11 @@
-#
-# MIT License
-#
-# Copyright (c) 2021 Carson Herrington
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
-#====================================================================================================
-# KhajiitBot - NotArtyom - 2021
-# ----------------------------------------
-# Command functions
-#====================================================================================================
 
 $bot.command :help do |event, *type|			# Help command
 	type = type.join(" ")
 
 	return event.send_embed do |embed|				# Send embedded help message
-		embed.thumbnail = Discordrb::Webhooks::EmbedImage.new(url: 'http://i.imgur.com/pG3L2RP.png')
-		embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: 'KhajiitBot', url: 'https://discordapp.com/oauth2/authorize?client_id=452660556990644225&scope=bot&permissions=0', icon_url: 'http://i.imgur.com/pG3L2RP.png')
-		if (type.empty?)
+
+	
 			embed.add_field(name: 'k.help usage:', value: IO.read("./ext/help/meta").force_encoding("utf-8"))
 		else
 			if (File.file?("./ext/help/#{type}"))
@@ -435,8 +407,7 @@ $bot.command :'e9.blacklist' do |event, action, *tags|
 		embed.color = EMBED_MSG_COLOR
 	end
 end
-
-#============================================ INTERNAL ==============================================
+INTERNAL 
 
 $bot.command(:usermod, max_args: 2, min_args: 2) do |event, target, level|		# USERMOD Command
 	target = Parser.get_user(target).id
@@ -462,4 +433,4 @@ $bot.command(:listmod, max_args: 0) do |event|									# LISTMOD Command
 	return nil
 end
 
-#====================================================================================================
+
