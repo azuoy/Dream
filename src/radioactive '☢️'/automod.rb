dd0228a767f2cc77c8fbd4f677fbbcb0853897a4
$o <100600051+azuoy@users.noreply.github.com>
2022-03-19 11:43:33 -0700

    Update automod.rb

--- src/radioactive '☢️'/automod.rb
+++ src/radioactive '☢️'/automod.rb
@@ -1,10 +1,23 @@
- "somehow manages to kiss themselves."
- "kisses themselves. How did they do that!?"
- "sets up for a kiss, but their lips somehow meet with their own face."
- "gives a big ol kiss to **#{target}**!"
- "attacks **#{target}** with a swarm of kisses!"
- "sends a kiss to **#{target}** via USPS. It never arrived..."
- "gives a big ol' kiss to **#{target}**!"
- "attacks **#{target}** with a swarm of kisses!"
- "creates unnecesary tension by kissing **#{target}**!"
- "lays down a big moist smooch towards **#{target}**!"
+
+<configuration>
+    <appender name="FILE" class="ch.qos.logback.core.rolling.RollingFileAppender">
+        <rollingPolicy class="ch.qos.logback.core.rolling.TimeBasedRollingPolicy">
+            <!-- daily rollover -->
+            <fileNamePattern>logs/log.%d{yyyy-MM-dd}.txt</fileNamePattern>
+
+            <!-- keep 30 days' worth of history capped at 1GB total size -->
+            <maxHistory>30</maxHistory>
+            <totalSizeCap>1GB</totalSizeCap>
+        </rollingPolicy>
+        <encoder>
+            <!-- author: Kaidan Gustave  -->
+            <pattern> 
+                %nopex[%d{HH:mm:ss}] [%level] [%logger{0}]: %msg%n%ex
+            </pattern>
+        </encoder>
+    </appender>
+
+    <root level="INFO">
+        <appender-ref ref="FILE"/>
+    </root>
+</configuration>
