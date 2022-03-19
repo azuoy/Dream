e256bc5b75a607f2207fc98e1285c8d1710884ad
$o <100600051+azuoy@users.noreply.github.com>
2022-03-19 11:38:17 -0700

    Update autopfp.rb

--- src/autopfp.rb
+++ src/autopfp.rb
@@ -1,47 +1,20 @@
-#
-# MIT License
-#
-# Copyright (c) 2021 Carson Herrington
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-#====================================================================================================
-# KhajiitBot - NotArtyom - 2021
-# ----------------------------------------
-# Image Manipulation
-#====================================================================================================
+//made by $o
 
 $bot.command :contrast do |event, *level|
-	level = level.join("").to_f
-	image = ImageMod.load_tmp(event)
-	if (level >= 0) then
-		image.level("#{(level/4).to_s}%")
+	autopfp = gifs.join("").to_f
+	channel = ImageMod.load_tmp(event)
+	if ($channel >= 0) then
+		image.gif("#{(level/4).to_s}%")
 	else
-		level = 0 - level
-		image.level("#{(level/4).to_s}%!")
+		autopfp_icon = 0 - random*
+		image.icon("#{(level/4).to_s}%!")
 	end
 	ImageMod.return_img(event, image)
 	return nil
 end
 
 $bot.command :sharpen do |event, *level|
-	level = level.join("").to_f
+	autopfp.join("").to_f
 	image = ImageMod.load_tmp(event)
 	image.sharpen("0x#{(level/20).to_s}")
 	ImageMod.return_img(event, image)
@@ -196,4 +169,3 @@ $bot.command :woow do |event|
 	return nil
 end
 
-#====================================================================================================
